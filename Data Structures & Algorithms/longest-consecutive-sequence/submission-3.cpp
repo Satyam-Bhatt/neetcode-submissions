272473ea97c2072b853cfaa12169de6869fe0b5d
Satyam Bhatt <63548966+Satyam-Bhatt@users.noreply.github.com>
2026-06-17 13:19:15 +0530

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.cpp
@@ -0,0 +1,39 @@
+class Solution {
+   public:
+    int longestConsecutive(vector<int>& nums) {
+        std::set<int> numsStore;
+
+        for (int n : nums) {
+            numsStore.insert(n);
+        }
+
+        int highestCount = 0;
+        int chain = 0;
+        int previousNumber = 0;
+        bool newCount = true;
+        for (int n : numsStore) {
+            if (newCount) {
+                chain = 1;
+                previousNumber = n;
+                newCount = false;
+
+                continue;
+            }
+
+            if (n == previousNumber + 1) {
+                chain += 1;
+                previousNumber = n;
+                continue;
+            } else {
+                if (chain > highestCount) highestCount = chain;
+                chain = 1;
+                previousNumber = n; 
+                newCount = false;
+            }
+        }
+
+        if (highestCount == 0 || chain > highestCount) return chain;
+
+        return highestCount;
+    }
+};
