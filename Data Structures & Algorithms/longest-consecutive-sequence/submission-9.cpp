a1abf411cd892e5fb2f739bb7f6f0aee9b0aa691
Satyam Bhatt <63548966+Satyam-Bhatt@users.noreply.github.com>
2026-06-17 13:44:51 +0530

    Add: longest-consecutive-sequence - submission-9

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-9.cpp
@@ -0,0 +1,32 @@
+// Time and space complexity O(n) - Neetcode solution
+// Check for the left neighbour and see is it a start of sequence or not
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        std::unordered_set<int> numsSet;
+        int length = 0;
+        int longest = 0;
+
+        for(int n : nums)
+        {
+            numsSet.insert(n);
+        }
+
+        for(int n : numsSet)
+        {
+            if(numsSet.find(n-1) == numsSet.end())
+            {
+                length = 1;
+                while(numsSet.find(n + length) != numsSet.end())
+                {
+                    length += 1;
+                }
+                if(length > longest)
+                    longest = length;
+            }
+        }
+
+        return longest;
+    }
+};
