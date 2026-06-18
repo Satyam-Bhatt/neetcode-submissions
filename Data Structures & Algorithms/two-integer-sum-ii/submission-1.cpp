82b13006db9f0ed0d9ef793410d2d3ea11ede285
Satyam Bhatt <63548966+Satyam-Bhatt@users.noreply.github.com>
2026-06-18 13:49:32 +0530

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+   public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+
+        for (int i = 0; i < numbers.size(); i++) {
+            int pointer = i + 1;
+
+            for(int j = i + 1; j < numbers.size(); j++)
+            {
+                //std::cout << numbers[i] << " || J " << numbers[j] << std::endl;
+
+                if(numbers[i] + numbers[j] > target || numbers[j] > target) break;
+                else if(numbers[i] + numbers[j] == target) return{i + 1, j + 1};
+            }
+
+            // while (numbers[i] + numbers[pointer] < target || numbers[pointer] < target) {
+            //     pointer++;
+            // }
+
+            // if (numbers[i] + numbers[pointer] == target)
+            //     return {numbers[i], numbers[pointer]};
+        }
+
+        return {0,0};
+    }
+};
