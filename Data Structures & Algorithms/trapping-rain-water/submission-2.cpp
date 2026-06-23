c5a616aad668f763bb20f8ff82119bb1c8ca925e
Satyam Bhatt <63548966+Satyam-Bhatt@users.noreply.github.com>
2026-06-23 12:18:44 +0530

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+   public:
+    int trap(vector<int>& height) {
+        int leftPointer = 0;
+        int rightPointer = height.size() - 1;
+        int leftMax = height[leftPointer];
+        int rightMax = height[rightPointer];
+        int water = 0;
+
+        while (leftPointer < rightPointer) {
+            if (height[leftPointer] <= height[rightPointer]) {
+                int val = leftMax - height[leftPointer];
+                if (val > 0) water = water + val;
+
+                if (height[leftPointer] > leftMax) leftMax = height[leftPointer];
+
+                leftPointer++;
+            } else if (height[rightPointer] < height[leftPointer]) {
+                int val = rightMax - height[rightPointer];
+                if (val > 0) water = water + val;
+
+                if (height[rightPointer] > rightMax) leftMax = height[rightPointer];
+
+                rightPointer--;
+            }
+        }
+
+        return water;
+    }
+};
