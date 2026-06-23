046426373bfa09e92eb7ba2d98d911cc18c04884
Satyam Bhatt <63548966+Satyam-Bhatt@users.noreply.github.com>
2026-06-23 13:50:00 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+   public:
+    bool isValid(string s) {
+        unordered_map<char, char> match = {{')', '('}, {'}', '{'}, {']', '['}};
+        stack<char> parentheses;
+
+        for (char c : s) {
+            if (match.count(c)) {  // it's a closing bracket
+                if (parentheses.empty() || parentheses.top() != match[c]) return false;
+                parentheses.pop();
+            } else {
+                parentheses.push(c);
+            }
+        }
+
+        return parentheses.empty();
+    }
+};
