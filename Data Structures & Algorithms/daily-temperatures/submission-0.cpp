fdda6ad2883ad969c15b417f5f37b6f02425f4bb
Satyam Bhatt <63548966+Satyam-Bhatt@users.noreply.github.com>
2026-06-26 14:03:58 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<vector<vector<int>>> check;
+
+        check.push({ {0}, {0} });
+
+        return {0,0};
+    }
+};
