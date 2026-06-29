2233e64cc3cf317b54b2d13fb66be70a12913711
Satyam Bhatt <63548966+Satyam-Bhatt@users.noreply.github.com>
2026-06-29 14:10:35 +0530

    Add: car-fleet - submission-6

---
+++ Data Structures & Algorithms/car-fleet/submission-6.cpp
@@ -0,0 +1,22 @@
+// This stores the time instead of position in the stack. This makes the code compact as you don't need to calculate time of the previous one by looking up the value in the hashmap
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+    map<int, int, greater<int>> posSpeed;
+    
+    for (int i = 0; i < position.size(); i++)
+        posSpeed[position[i]] = speed[i];
+
+    stack<float> st;
+
+    for (auto [pos, spd] : posSpeed) {
+        float time = (float)(target - pos) / spd;
+        // Only forms a new fleet if it can't catch the one ahead
+        if (st.empty() || time > st.top())
+            st.push(time);
+        // else: merges into the fleet ahead, don't push
+    }
+
+    return st.size();
+}
+};
